4cb95fe5e1e38744e2e30198d2c97062ba106f88
demigod <divyanshjain.2206@gmail.com>
2024-10-30 11:52:28 +0530

    update contract

--- src/app/api/contract/getContract.ts
+++ src/app/api/contract/getContract.ts
@@ -1,7 +1,7 @@
 import { OasisSapphireTestnet } from "./OasisSapphireTestnet";
 import { ethers, verifyMessage, Wallet } from "ethers";
 import contractABI from "./contractABI.json" assert { type: "json" };
-const contractAddress = "0xb41175aa3ff245c29BE0EC53AB37c8c319259daA";
+const contractAddress = "0x141609F9B466b55736cBB4C680984Aef0358abe6";
 
 let Contract: ethers.Contract | null = null;
 
@@ -9,15 +9,24 @@ if (!process.env.PRIVATE_KEY) {
     throw new Error('Invalid/Missing environment variable: "PRIVATE_KEY"')
 }
 
+// export default async function GetContract(){
+//     if (Contract == null) {
+//         const url = OasisSapphireTestnet.rpcUrls.public.http[0];
+//         const privateKey = '0x' + process.env.PRIVATE_KEY;
+//         const signer = await new ethers.Wallet(privateKey);
+//         const provider = await new ethers.JsonRpcProvider(url);
+//         const wallet = signer.connect(provider);
+//         console.log(signer.address);
+//         Contract = new ethers.Contract(contractAddress, contractABI, wallet);
+//     }
+//     return Contract;
+// }
+
 export default async function GetContract(){
     if (Contract == null) {
-        const url = OasisSapphireTestnet.rpcUrls.public.http[0];
-        const privateKey = '0x' + process.env.PRIVATE_KEY;
-        const signer = await new ethers.Wallet(privateKey);
-        const provider = await new ethers.JsonRpcProvider(url);
-        const wallet = signer.connect(provider);
-        console.log(signer.address);
-        Contract = new ethers.Contract(contractAddress, contractABI, wallet);
+        const ethersProvider = new ethers.BrowserProvider(window.ethereum); 
+        const signer = await ethersProvider.getSigner()
+        Contract = new ethers.Contract(contractAddress, contractABI, signer);
     }
     return Contract;
 }
--- src/app/front/page.tsx
+++ src/app/front/page.tsx
@@ -1,16 +1,71 @@
 'use client'
 import { useState, useEffect } from "react"
-import { useAppKitAccount, useAppKit } from "@reown/appkit/react";
 import { useRouter } from 'next/navigation'
-
+import { useAppKitProvider, useAppKitAccount } from "@reown/appkit/react"
+import { ethers, BrowserProvider, Contract, formatUnits } from 'ethers'
+import { Button } from "@/components/ui/button"
+import contractABI from "../../contract/abi.json" assert { type: "json" };
+import contractAddress from "../../contract/address.json" assert { type: "json" };
 export default function Front() {
   const router = useRouter()
   const { address, isConnected, caipAddress, status } = useAppKitAccount()
   const [greeting, setGreeting] = useState("");
+  const { walletProvider } = useAppKitProvider()
 
   useEffect(() => {
     console.log("Connected: ", isConnected, address, caipAddress, status)
   },[])
+
+  const getContract = async () => {
+    if(!isConnected) {
+      console.log('Not connected!');
+      return;
+    }
+    const ethersProvider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await ethersProvider.getSigner()
+    const contract = new ethers.Contract(contractAddress.address, contractABI, signer);
+    console.log(contract)
+    console.log(signer.address);
+    const messageHash = await contract.getMessageHash(signer.address, 1);
+    console.log("msg ", messageHash)
+    let flatSig = await signer.signMessage(ethers.getBytes(messageHash));
+    console.log("sign ", flatSig);
+    
+    const res = await contract.verify(signer.address, 1, flatSig);
+    console.log("verify ", res);
+  }
+
+
+  const getSignature = async () => {
+    if(!isConnected) {
+      console.log('Not connected!');
+      return;
+    }
+    // const ethersProvider = new BrowserProvider(walletProvider)
+    const ethersProvider = new ethers.BrowserProvider(window.ethereum); 
+    const signer = await ethersProvider.getSigner()
+    console.log(signer);
+    console.log(ethersProvider)
+
+    // console.log(signer)
+    // Sign the string message
+    
+    const messageHash = ethers.solidityPackedKeccak256(
+      ["uint256", "uint256", "address"],
+      [1, 1, address.toString()]
+    );
+    
+    // console.log(messageHash);
+    let flatSig = await signer.signMessage(messageHash);
+    // console.log("Signature: ", flatSig);
+    
+    let sig = ethers.Signature.from(flatSig);
+    // console.log(sig);
+    console.log(ethers.verifyMessage(messageHash, flatSig));
+    // `signMessage` in ethers.js expects a Uint8Array, so use `arrayify`
+    // const signature = await walletClient.signMessage(ethers.getBytes(messageHash));
+    // console.log("Generated signature:", signature);
+  }
   
   const saveGreeting = ()=>{
     fetch('/api',{
@@ -24,7 +79,8 @@ export default function Front() {
     <div>
       <input value={greeting} onChange={(e)=>setGreeting(e.target.value)}/>
       <button  onClick={saveGreeting}>send my greeting to the server</button>
-      <w3m-button/>
+      <Button onClick={getContract}>Sign Message</Button>
+      <w3m-account-button/>
     </div>
   )
 }
